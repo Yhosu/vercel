f9689ad75319217ecf7426208a76ce48526ced5b
yhosug7 <jgutierrez@solunes.com>
2025-09-06 21:45:18 -0400

    Test iframe

---
+++ resources/views/content/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- OwnerRez Booking/Inquiry widget for Enchanted Rose Villa - 4450 Shiva Loop -->
+    <div class="ownerrez-widget" data-propertyId="0713b741e1e24a1ca3ddaca3fbdf2906" data-widget-type="Booking/Inquiry" data-widgetId="65ba391d41c44d21914038729acbfa27"></div>
+    <script src="https://app.ownerrez.com/widget.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/test-queries', function () {
+    return view('content.test');
+});
